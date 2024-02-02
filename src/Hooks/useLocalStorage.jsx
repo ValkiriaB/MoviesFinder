a5a510b4e68d495788e6a1427c0c3e55d6869f28
Valeria <valeriabarki@gmail.com>
2024-02-02 15:59:37 -0300

    first commit

---
+++ src/Hooks/useLocalStorage.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+    } catch (error) {
+      console.error("Error retrieving data from localStorage:", error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Error storing data in localStorage:", error);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
